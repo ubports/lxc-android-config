bae870aa54b40b881fccbd579ff98a275ce3f60c
Dalton Durst <me@daltondurst.com>
2018-03-09 16:41:31 -0600

    Can't use dashes in function names

--- usr/bin/ssh-keygen.sh
+++ usr/bin/ssh-keygen.sh
@@ -3,7 +3,7 @@
 # Sometimes /etc/ssh isn't writable when the ssh job starts. Also, for some 
 # reason we can't rely on the 'mounted /etc/ssh' event, it doesn't always fire.
 # This script works around both of those problems.
-wait-for-writable() {
+wait_for_writable() {
     while [ ! $WRITABLE ]; do
         touch /etc/ssh/testfile && {
             rm /etc/ssh/testfile
@@ -15,10 +15,10 @@ wait-for-writable() {
 }
 
 if [ ! -e /etc/ssh/ssh_host_rsa_key ]; then
-    wait-for-writable
+    wait_for_writable
     ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
 fi
 if [ ! -e /etc/ssh/ssh_host_dsa_key ]; then
-    wait-for-writable
+    wait_for_writable
     ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
 fi
