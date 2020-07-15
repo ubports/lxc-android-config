bf7c2dc380543dc9369f1ce8bb4b478729727636
Dalton Durst <dalton@ubports.com>
2020-07-15 11:40:55 -0500

    Update QtWebEngine's env for disabling viz
    The Viz compositor causes major performance problems with CPU rendering
    of Chromium. In particular, Chromium's framerate appears to go much
    faster than the software texture upload can handle, causing a
    stuttering mess. We can get around this in every version of QWE up to
    5.14 by disabling viz.

--- etc/profile.d/disable-qtwebengine-gpu.sh
+++ etc/profile.d/disable-qtwebengine-gpu.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Disable the GPU for QtWebEngine on Android-based devices
 # This file is part of lxc-android-config
-export QTWEBENGINE_CHROMIUM_FLAGS="--disable-gpu"
+export QTWEBENGINE_CHROMIUM_FLAGS="--disable-gpu --disable-viz-display-compositor"
